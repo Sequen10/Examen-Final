554c3aea3765f0ecfd46393495c2c46efbbe9706
Sequen10 <gsequenm@miumg.edu.gt>
2026-06-01 14:51:23 -0600

    rutaspeliculas

--- app/routers/router.js
+++ app/routers/router.js
@@ -3,8 +3,7 @@ let router = express.Router();
 
 //Importar tablas
 const juegos = require('../controllers/juego.controller.js');
-// 👇 Ahora se llama "peliculas", igual que "juegos"
-const peliculas = require('../controllers/pelicula.controller.js'); 
+const peliculas = require('../controllers/pelicula.controller.js');
 
 //Tabla usuarios (Juegos)
 router.post('/api/juegos/create', juegos.create);
@@ -12,7 +11,7 @@ router.get('/api/juegos/onebyid/:id', juegos.getJuegoById);
 router.put('/api/juegos/update/:id', juegos.updateById);
 router.delete('/api/juegos/delete/:id', juegos.deleteById);
 
-//Tabla Películas 👇 (Mismo estilo exacto)
+//Tabla Películas
 router.post('/api/peliculas/create', peliculas.create);
 router.get('/api/peliculas/onebyid/:id', peliculas.getPeliculaById);
 router.put('/api/peliculas/update/:id', peliculas.updateById);
